9c48c5e0b30512448ea5f8281068be8029e7a91e
Drunkenpilot <bobhao@gmail.com>
2018-02-21 13:24:59 +0100

    update

--- www/posprintermanager.js
+++ www/posprintermanager.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "posprintermanager", "coolMethod", [arg0]);
+exports.buildImage = function(arg0, success, error) {
+    exec(success, error, "posprintermanager", "buildImage", [arg0]);
 };
